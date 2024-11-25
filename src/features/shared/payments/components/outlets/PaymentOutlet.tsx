86e94f4e5a65b481fe3c6c7c025f059ec2991787
eternalramenz <147462463+eternalramenz@users.noreply.github.com>
2024-11-25 16:09:51 +0800

    Update PaymentOutlet.tsx

--- src/features/shared/payments/components/outlets/PaymentOutlet.tsx
+++ src/features/shared/payments/components/outlets/PaymentOutlet.tsx
@@ -1,14 +1,16 @@
+import { useState } from 'react'
 import PaymentHeader from '../PaymentHeader'
 import PaymentDataTable from '../table/PaymentDataTable'
 
 const PaymentOutlet = () => {
 
+  const [ searchValue, setSearchValue ] = useState<string>('')
   
   return (
     <main className="h-full bg-white flex flex-col">
-      <PaymentHeader />
+      <PaymentHeader onChange={setSearchValue} />
       <section className="flex-grow overflow-y-hidden">
-        <PaymentDataTable />
+        <PaymentDataTable searchValue={searchValue} />
       </section>
     </main>
   )
