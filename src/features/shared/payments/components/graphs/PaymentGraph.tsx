5169b7a267d5e331e75320e7a068a46854bfcf7e
eternalramenz <147462463+eternalramenz@users.noreply.github.com>
2024-11-25 23:57:42 +0800

    Update PaymentGraph.tsx

--- src/features/shared/payments/components/graphs/PaymentGraph.tsx
+++ src/features/shared/payments/components/graphs/PaymentGraph.tsx
@@ -1,12 +1,7 @@
-import { PaymentAnalyticsType } from "../../types/payment.types";
-import PlusIcon from "@icons/linear/PlusIcon";
 import MoneyIcon from "@icons/linear/MoneyIcon";
-import DownTrendIcon from "@icons/linear/DownTrendIcon";
-import UpTrendIcon from "@icons/linear/UpTrendIcon";
-import PhilippinePesoIcon from "@icons/linear/PhilippinePesoIcon";
 
 type PaymentGraphProps = {
-  data: PaymentAnalyticsType
+  data: number
 }
 
 const PaymentGraph = ({
@@ -15,51 +10,24 @@ const PaymentGraph = ({
   
 
 
-  const revenue = data?.revenue.value || 0
-  const isRevenueIncreasing = data?.revenue.isUpTrend || false
-  const revenuePercentage = data?.revenue.percentage || 0
-
   return (
-    <article
-     className="h-full w-full flex items-center justify-start gap-4 p-6 "
-    >
-      <section className="flex items-start justify-start h-full">
-        <figure className="w-10 h-10 rounded-full bg-gray-100 flex p-1 items-center justify-center">
-          <figure className=" rounded-full p-1 flex items-center justify-center">
-            <MoneyIcon className="w-5 h-5 stroke-2 stroke-gray-500" />
-          </figure>
-        </figure>
-      </section>
+    <header className="h-24 w-full flex items-center justify-start gap-4 p-6 ">
+    <section className="flex items-start justify-start h-full">
+      <figure className="w-10 h-10 rounded-full bg-lime-50 flex p-1 items-center justify-center">
+        <MoneyIcon className="w-6 h-6 stroke-2 stroke-lime-500" />
+      </figure>
+    </section>
+    <section className="flex items-center justify-start h-full">
       <section className="flex flex-col items-start gap-1 h-full">
-        <label className="text-xs font-medium tracking-wide text-gray-500">REVENUE THIS MONTH</label>
-        <div className="flex items-center justify-center">
-          <PhilippinePesoIcon className="stroke-2 stroke-gray-900 w-8 h-8" />
-          <div
-            className="flex items-center gap-2"
-          >
-            <h1 className="text-3xl font-medium tracking-tight text-gray-900">{(revenue).toLocaleString('en-US')}</h1>
-            {isRevenueIncreasing ? (
-              <div className="flex items-center justify-center px-2 py-1 bg-green-50 rounded-full">
-                <UpTrendIcon className="w-4 h-4 stroke-2 stroke-green-500" /> 
-                <p className="text-xs tracking-wide text-green-500 flex items-center ml-1">
-                  {revenuePercentage}
-                  <PlusIcon className="w-3 h-3 stroke-2 stroke-green-500" />
-                </p>
-              </div>
-            ) : (
-              <div className="flex items-center justify-center px-2 py-1 bg-rose-50 rounded-full">
-                <DownTrendIcon className="w-5 h-5 stroke-2 stroke-red-500" />
-                <p className="text-xs tracking-wide text-red-500 flex items-center ml-1">
-                  {revenuePercentage}
-                  <PlusIcon className="w-3 h-3 stroke-2 stroke-red-500" />
-                </p>                
-              </div>
-            )} 
-          </div>
-        </div>
+        <label className="text-xs font-medium tracking-wide text-gray-500 uppercase">
+          TOTAL PAYMENTS
+        </label>
+        <h1 className="text-3xl font-medium tracking-tight text-gray-900">
+          {data}
+        </h1>
       </section>
-
-    </article>
+    </section>
+  </header>
   )
 }
 
