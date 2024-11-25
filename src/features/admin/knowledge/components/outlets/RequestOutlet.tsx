b79b8163b055463ef42c00a1993b07e30e3f9dbe
eternalramenz <147462463+eternalramenz@users.noreply.github.com>
2024-11-25 15:58:51 +0800

    Update RequestOutlet.tsx

--- src/features/admin/knowledge/components/outlets/RequestOutlet.tsx
+++ src/features/admin/knowledge/components/outlets/RequestOutlet.tsx
@@ -1,15 +1,17 @@
+import { useState } from "react"
 import KnowledgeHeader from "../KnowledgeHeader.tsx"
 import RequestDataTable from "../table/request/RequestDataTable.tsx"
 
 
 const RequestOutlet = () => {
 
+  const [searchValue, setSearchValue] = useState<string>("")
 
   return (
     <main className="h-full bg-white flex flex-col">
-      <KnowledgeHeader />
+      <KnowledgeHeader onChange={setSearchValue} />
       <section className="flex-grow overflow-y-hidden ">
-        <RequestDataTable />
+        <RequestDataTable searchValue={searchValue} />
       </section>
     </main>
 
