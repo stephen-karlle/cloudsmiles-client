38806918a8f2a22f8526573ff690041298b04f82
eternalramenz <147462463+eternalramenz@users.noreply.github.com>
2024-11-25 14:04:40 +0800

    Create ActivityIcon.tsx

---
+++ src/icons/linear/ActivityIcon.tsx
@@ -0,0 +1,10 @@
+
+const ActivityIcon = ({ className }: any) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" className={className}>
+      <path d="M22 12H18L15 21L9 3L6 12H2" strokeLinecap="round" strokeLinejoin="round"/>
+    </svg>
+  );
+};
+
+export default ActivityIcon;
