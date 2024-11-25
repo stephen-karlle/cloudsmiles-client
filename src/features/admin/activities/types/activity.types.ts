b913a5c0fbe87fc9bf3e82e96e6f362025c218f9
eternalramenz <147462463+eternalramenz@users.noreply.github.com>
2024-11-25 15:51:22 +0800

    Update activity.types.ts

--- src/features/admin/activities/types/activity.types.ts
+++ src/features/admin/activities/types/activity.types.ts
@@ -1,8 +1,7 @@
 import { AssistantResponseType } from "@features/admin/staff/types/assistant.types";
 
-
-
 export type ActivityType = {
+  activitySerialId: string;
   activityAssistantId: AssistantResponseType
   activityDescription: string;
   activityAction: string;
